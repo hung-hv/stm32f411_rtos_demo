03d7c52bba5a1dc1afad47cfdc5a6f3829297b1e
hung-hv <viethung170230@gmail.com>
2024-01-03 19:09:22 +0700

    stm32f411 with HSI config

---
+++ stm32f411_rtos_demo1/Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/list.h
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/HAU5HC/Documents/stm32/stm32_rtos/stm32f4xx_rtos/stm32f411_rtos_demo1/ThirdParty/FreeRTOS/include/list.h:
